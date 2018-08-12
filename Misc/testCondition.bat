74b9a21ac267285611a18d64029df6e2f3d53dd9
Samuelimza <osamaazmi1999@gmail.com>
2018-08-13 00:03:17 +0530

    automatedCommit

--- Misc/testCondition.bat
+++ Misc/testCondition.bat
@@ -14,7 +14,7 @@ set /p commit=Do you want to commit(Y/N)?
 IF %commit%==Y (
 	git commit -m "automatedCommit"
 ) ELSE (
-	echo if dont wanna commit then fuckoff
+	echo If you didn't want to commit then why Add?, FUCKOFF!!
 	PAUSE
 	exit
 )
